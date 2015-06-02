582d8369942227f4572145ac6f12cb5a5c433198
Masataka.Ugumori <Masataka.Ugumori@jp.sony.com>
2015-06-02 14:56:45 +0900

    6.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com",
+  before do 
+  	@user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
@@ -75,4 +77,20 @@ describe User do
   		before { @user.password_confirmation = "mismatch" }
   		it { should_not be_valid }
 	end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it {should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) {found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
 end
