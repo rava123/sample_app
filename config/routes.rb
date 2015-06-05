e18941d9af9b4dcdf2c19b3d90e2788f32ac89cc
Masataka.Ugumori <Masataka.Ugumori@jp.sony.com>
2015-06-05 20:44:36 +0900

    8.25

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  root 'static_pages#home'
+  
   match '/signup',   to: 'users#new',            via: 'get'
+  match '/signin',   to: 'sessions#new',         via: 'get'
+  match '/signout',  to: 'sessions#destroy',     via: 'delete'
+  
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
@@ -9,7 +15,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,13 @@ describe "User pages" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)  			
 			end
+
+			describe "after submission" do
+				before { click_button submit}
+
+				it { should have_title('Sign up')}
+				it { should have_content('error')}
+			end
 		end
 
 		describe "with valid information" do
@@ -42,6 +49,8 @@ describe "User pages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
+
+			
 		end
 	end
 end
